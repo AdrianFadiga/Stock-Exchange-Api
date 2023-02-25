a9f3e4349fd09d1e5c7a8b36f58848d46dd10b8d
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-02-25 16:10:42 -0300

    init project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+
+const port = 3000;
+
+app.use("/", router);
+
+app.listen(port, () => {
+	console.log(`O pai ta on na porta ${port}`);
+});
