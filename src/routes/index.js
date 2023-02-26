43764835ce2cebf2f45ecea1b013e32362b87f14
Adrian Martins Fadiga <adrianmartinsfadiga@pop-os.localdomain>
2023-02-25 23:01:28 -0300

    feat: rota de findMany stock exchanges

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
+const {StockExchangeController} = require("../controllers");
+
+router.get("/stockexchanges", StockExchangeController.getAll);
 
 module.exports = router;
